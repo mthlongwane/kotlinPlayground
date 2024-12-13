048b0e2e08268dad02d0453ebc12815864cce61b
Macbook <macbook@emmteees-MacBook-Pro.local>
2024-12-13 23:20:45 +0200

    Kotlin function called in java code.

--- gradleKotlinCMD/app/build.gradle.kts
+++ gradleKotlinCMD/app/build.gradle.kts
@@ -4,14 +4,22 @@
 
 plugins {
     id("buildlogic.java-application-conventions")
+    kotlin("jvm") version "2.1.0"
 }
 
 dependencies {
     implementation("org.apache.commons:commons-text")
     implementation(project(":utilities"))
+    implementation(kotlin("stdlib"))
 }
 
 application {
     // Define the main class for the application.
     mainClass = "org.example.app.App"
+}
+repositories {
+    mavenCentral()
+}
+kotlin {
+    jvmToolchain(17)
 }
--- gradleKotlinCMD/app/src/main/java/org/example/app/App.java
+++ gradleKotlinCMD/app/src/main/java/org/example/app/App.java
@@ -5,17 +5,29 @@ package org.example.app;
 
 import org.example.list.LinkedList;
 
+import static org.example.app.KotlinTestKt.banter;
 import static org.example.utilities.StringUtils.join;
 import static org.example.utilities.StringUtils.split;
 import static org.example.app.MessageUtils.getMessage;
 
+
+import java.util.logging.Logger;
+
 import org.apache.commons.text.WordUtils;
 
 public class App {
+
+
     public static void main(String[] args) {
+        Logger logger = Logger.getLogger("Main ");
+
+        logger.info(banter());
+
         LinkedList tokens;
         tokens = split(getMessage());
         String result = join(tokens);
-        System.out.println(WordUtils.capitalize(result));
+
+        logger.info(WordUtils.capitalize(result));
+
     }
 }
