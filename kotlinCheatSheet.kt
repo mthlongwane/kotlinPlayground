18b9708e4dcc3a2f1f4878dbbdc207e37aeba757
mthlongwane <thandotmh@gmail.com>
2024-12-14 23:18:33 +0200

    playing with enums

--- kotlinCheatSheet.kt
+++ kotlinCheatSheet.kt
@@ -1,3 +1,4 @@
+import Color.*
 /**
  * reference: Kotlin in Action - Dmitry Jemerov and Svetlana Isakova
  * Example code: 
@@ -7,7 +8,8 @@
  */
 
 fun main(){
-    namedGreeting()
+//    namedGreeting()
+    playWithEnums()
 }
 
 fun namedGreeting(){
@@ -20,7 +22,6 @@ fun namedGreeting(){
 
     val person1 =  Person(name,age)
     println("Adult status ?: ${person1.isAdult}")
-
 }
 /**
  * Concise Kotlin Class
@@ -37,4 +38,53 @@ class Person( //Public class by Default
         get(){ // custom accessors allow you to override the default accessor logic.
             return age>18 //computed
         }
+}
+
+//enum class Color{ // Standard Enum class with no constructor
+//    RED, GREEN, BlUE,YELLOW, INDIGO, VIOLET
+//}
+fun playWithEnums(){
+    println(Color.BLUE.rgb)     //Computed Enum Value
+    Color.GREEN.printColour()   //Enum Function
+
+    println(mixColours(Color.RED, Color.YELLOW))
+    println(mixColours(Color.VIOLET, Color.YELLOW))
+}
+
+/**
+ * An enum class representing common colors, each with its RGB (Red, Green, Blue) values.
+ *
+ * Each color is associated with red, green, and blue components, and an integer value
+ * representing its combined RGB color code.
+ *
+ * @property r The red component of the color (0-255).
+ * @property g The green component of the color (0-255).
+ * @property b The blue component of the color (0-255).
+ * @property rgb The calculated RGB color code as an integer.
+ */
+enum class Color(
+   val r: Int,
+   val g: Int,
+   val b: Int
+){
+    RED(255, 0, 0), GREEN(0, 255, 0), BLUE(0, 0, 255), YELLOW(255, 255, 0), ORANGE(255, 165, 0), INDIGO(75, 0, 130), VIOLET(238, 130, 238);
+    //Semicolon separates the enum constant list from the method definitions
+    val rgb = (r * 256 + g) * 256 + b
+    fun printColour() = println("$this is $rgb")
+}
+
+
+/**
+ * Function for mixing two colours and returning a colour using a when statement
+ *
+ * @param color1
+ * @param color2
+ */
+fun mixColours(color1:Color, color2:Color): Color{
+    return when (setOf(color1,color2)){
+        setOf(Color.RED, Color.YELLOW) -> Color.ORANGE //If the Enum is imported you dont need to use the Enum Name.
+        setOf(BLUE, YELLOW) -> GREEN
+        setOf(BLUE, VIOLET) -> INDIGO
+        else -> throw Exception("Unkown colour mix")
+    }
 }
